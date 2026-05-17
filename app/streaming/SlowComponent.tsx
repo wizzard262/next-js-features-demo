06404ae606f94ad39cc8a6a41ac32a54f57ab96c
wizzard262 <jones.steve@live.com>
2026-05-17 16:31:47 +0100

    again

--- app/streaming/SlowComponent.tsx
+++ app/streaming/SlowComponent.tsx
@@ -1,13 +1,16 @@
 // A deliberately slow server component to demonstrate streaming.
 // It waits 3 seconds before rendering.
 
-export default async function SlowComponent() {
-  
-  // Simulate a slow operation (e.g., data fetching, heavy computation)
-  // but in production this is not honoured, we need to use a real async operation like a fetch or database query
-  // await new Promise((r) => setTimeout(r, 3000)); // 3-second delay
+import { headers } from "next/headers";
 
-  // This works on Vercel because it's treated as an internal server fetch(i.e. no aboslute URL needed) and is not cached, so it always hits the server and waits 3 seconds before responding
-  await fetch("/api/sleep", { cache: "no-store" });
+export default async function SlowComponent() {
+  const h = headers();
+  const host = h.get("host");
+  const protocol = h.get("x-forwarded-proto") || "https";
+
+  const url = `${protocol}://${host}/api/sleep`;
+
+  await fetch(url, { cache: "no-store" });
+
   return <p>Loaded after 3 seconds</p>;
 }
