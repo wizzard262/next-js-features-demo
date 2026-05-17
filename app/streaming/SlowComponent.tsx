2fa2cb2dd45ff6340bffc7b1171d0cbb77193d5d
wizzard262 <jones.steve@live.com>
2026-05-17 16:28:31 +0100

    again

--- app/streaming/SlowComponent.tsx
+++ app/streaming/SlowComponent.tsx
@@ -6,6 +6,8 @@ export default async function SlowComponent() {
   // Simulate a slow operation (e.g., data fetching, heavy computation)
   // but in production this is not honoured, we need to use a real async operation like a fetch or database query
   // await new Promise((r) => setTimeout(r, 3000)); // 3-second delay
-  await fetch("/api/sleep");
+
+  // This works on Vercel because it's treated as an internal server fetch(i.e. no aboslute URL needed) and is not cached, so it always hits the server and waits 3 seconds before responding
+  await fetch("/api/sleep", { cache: "no-store" });
   return <p>Loaded after 3 seconds</p>;
 }
